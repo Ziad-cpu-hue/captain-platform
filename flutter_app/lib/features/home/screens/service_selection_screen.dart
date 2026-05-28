11ed9f589bcaf548aefd8212bb77b979e8af34c5
Ziad-cpu-hue <code37265@gmail.com>
2026-05-28 13:20:19 +0300

    Delete flutter_app directory

--- flutter_app/lib/features/home/screens/service_selection_screen.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'home_screen.dart';
-class ServiceSelectionScreen extends StatelessWidget {
-  const ServiceSelectionScreen({super.key});
-  @override
-  Widget build(BuildContext context) => const HomeScreen();
-}
